b4af168b59fd0d314f8cc6434086b03ab0fb2f2a
Joselito17821 <josemanuelmany04@gmail.com>
2024-10-16 02:25:20 -0500

    Adding video-views exceptions

---
+++ src/main/java/culturemedia/exception/VideoNotFoundException.java
@@ -0,0 +1,11 @@
+package culturemedia.exception;
+
+public class VideoNotFoundException extends CultureMediaException {
+
+    public VideoNotFoundException() {
+        super("Video not found");
+    }
+    public VideoNotFoundException(String title) {
+        super("Video not found: " + title);
+    }
+}
