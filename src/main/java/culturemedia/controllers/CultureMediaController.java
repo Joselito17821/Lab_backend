4f6bbbe0d66ab3e9ec3c6fd2e3111bea5cd6d5ec
Joselito17821 <josemanuelmany04@gmail.com>
2024-11-20 20:50:19 -0500

    adding @CrossOrigin(origins = "*")

--- src/main/java/culturemedia/controllers/CultureMediaController.java
+++ src/main/java/culturemedia/controllers/CultureMediaController.java
@@ -6,12 +6,11 @@ import java.util.*;
 import culturemedia.exception.VideoNotFoundException;
 import culturemedia.model.Video;
 import culturemedia.service.CultureMediaService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
+@CrossOrigin(origins = "*")
 public class CultureMediaController {
 
     private final CultureMediaService cultureMediaService;
@@ -20,12 +19,16 @@ public class CultureMediaController {
         this.cultureMediaService = cultureMediaService;
     }
 
+
     @GetMapping("/videos")
+    @CrossOrigin(origins = "*")
     public List<Video> findAll() throws VideoNotFoundException {
         return cultureMediaService.findAll();
     }
 
+
     @PostMapping("/videos")
+    @CrossOrigin(origins = "*")
     public Video save(@RequestBody Video video) throws VideoNotFoundException {
         return this.cultureMediaService.save(video);
     }
